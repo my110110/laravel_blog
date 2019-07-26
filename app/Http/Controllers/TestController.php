acab9df5bb21816b2c1e85e72dbcf227a693ddc3
zhengyonghui <446910086@qq.com>
2019-07-26 14:55:44 +0800

    chore:  first

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -31,7 +31,37 @@ class TestController extends Controller
     
     
     public function send(){
-            echo PHP_EOL.'-'.'13';
-     }
+        $data= [
+            [
+                'province_id' => 1,
+                'city_id' =>3,
+                'country_id' => 0,
+                'town_id' => 0
+            ],
+            [
+        
+                'province_id' => 1,
+                'city_id' => 2,
+                'country_id' => 3,
+                'town_id' => 0
+            ],
+           
+        ];
+      
+     
+        //过滤掉值为空的数组元素
+        $temp = array_map('array_filter',$data);
+        //连接每个数组的城市
+        $temp = collect($temp)->map(function($item, $key){
+            return collect($item)->flatten()->implode('-');
+        });
+        //测试城市之间是否有包含关系，返回boole值true, false
+        $temp = $temp->map(function($item, $key) use($temp){
+            return \Illuminate\Support\Str::contains($item, $temp->except($key)->all());
+        })->contains(true);
+        
+        var_dump($temp);
+    
+    }
     
 }
